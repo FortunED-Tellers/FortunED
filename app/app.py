7f8363b6ddd1db395c92a61369050915a2fd408f
informidas <58525632+informidas@users.noreply.github.com>
2020-06-27 13:22:41 -0400

    Create app.py
    Initial Boilerplate code for flask app.py file

---
+++ app/app.py
@@ -0,0 +1,40 @@
+import os
+import json
+from flask import Flask, request, render_template, jsonify
+
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+from bson.json_util import dumps
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+dbconn = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = dbconn.fortunedb
+
+
+# Define routes
+@app.route("/")
+def welcome():
+    return render_template('index.html')
+
+@app.route("/team.html")
+def team():
+    return render_template('team.html')
+
+@app.route("/results.html/<county>")
+
+
+        return render_template('results.html', array=array)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
